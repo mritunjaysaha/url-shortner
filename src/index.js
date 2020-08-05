e15b7a666c5e734bffdf059665ff9eb231cf2a3b
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-08-05 21:04:35 +0530

    added rel.ink api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+/**
+ * @param
+ * @return hashid, the hashid is added to https://rel.ink/${hashid}
+ */
+async function shortly() {
+    const body = JSON.stringify({
+        url:
+            "https://www.frontendmentor.io/solutions/desktopfirst-shortly-landing-page-with-react-and-sass-wUwZMU7sl/preview",
+    });
+
+    const res = await fetch("https://rel.ink/api/links/", {
+        method: "POST",
+        body,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    console.log(await res.json());
+}
+
+// https://rel.ink/9yXG7L
+// https://rel.ink/${hashid}
+shortly();
