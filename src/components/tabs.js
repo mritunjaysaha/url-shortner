0cc9ed999202fde33d9bfb2cde33244df87f6173
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-08-13 13:41:52 +0530

    added tabs component

---
+++ src/components/tabs.js
@@ -0,0 +1,39 @@
+function Tabs() {
+    this.el = document.querySelector(".tabs-tab");
+    this.signin = document.querySelector("#signin");
+    this.signup = document.querySelector("#signup");
+
+    this.previousTab = document.querySelector(`article[data-val='signin']`);
+    console.log(this.el);
+
+    this.bindEvents();
+}
+
+Tabs.prototype.bindEvents = function () {
+    this.el.addEventListener("click", (e) => {
+        const tab = e.target.dataset["val"];
+        console.log(tab);
+        const selectedTab = document.querySelector(
+            `article[data-val='${tab}']`
+        );
+
+        if (tab === "signin") {
+            console.log("here", this.previousTab);
+            this.previousTab.classList.remove("selected-tab");
+
+            selectedTab.classList.add("selected-tab");
+            this.signin.style.display = "flex";
+            this.signup.style.display = "none";
+            this.previousTab = selectedTab;
+        } else {
+            console.log("here", this.previousTab);
+            this.previousTab.classList.remove("selected-tab");
+            selectedTab.classList.add("selected-tab");
+            this.signin.style.display = "none";
+            this.signup.style.display = "flex";
+            this.previousTab = selectedTab;
+        }
+    });
+};
+
+new Tabs();
