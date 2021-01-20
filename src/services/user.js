1ba70c67b4dc27be8ebc9e81e1371c2c572388e2
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2021-01-20 18:58:51 +0530

    removed backend

--- src/services/user.js
+++
@@ -1,7 +0,0 @@
-async function getUser() {
-    const response = await fetch("http://localhost:8000/api/user");
-    const processedResponse = await response.json();
-    console.log(processedResponse);
-}
-
-getUser();
