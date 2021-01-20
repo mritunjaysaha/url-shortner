9c9dcd571b88c3989e8b829286c62dcbc79b7db1
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2021-01-20 20:09:13 +0530

    updated hamburger icon

--- src/menu.js
+++ src/menu.js
@@ -1,24 +1,27 @@
-function Menu(el, button) {
+function Menu(el, button, icon, close) {
     this.el = document.querySelector(el);
     this.button = document.querySelector(button);
-
+    this.hamburgerMenuIcon = document.querySelector(icon);
+    this.closeIcon = document.querySelector(close);
     this.displayMenu = false;
-
+    console.log(this.hamburgerMenuIcon, this.closeIcon);
     this.bindEvents();
 }
 
 Menu.prototype.bindEvents = function () {
-    this.button.addEventListener("click", (e) => {
-        console.log("clicked");
-        // this.el.style.display = "flex";
+    this.button.addEventListener("click", () => {
         if (this.displayMenu === false) {
             this.el.style.display = "flex";
             this.displayMenu = true;
+            this.hamburgerMenuIcon.style.display = "none";
+            this.closeIcon.style.display = "block";
         } else {
             this.el.style.display = "none";
             this.displayMenu = false;
+            this.hamburgerMenuIcon.style.display = "block";
+            this.closeIcon.style.display = "none";
         }
     });
 };
 
-new Menu("#menu", "#hamburger");
+new Menu("#menu", "#hamburger", ".hamburger-icon", ".menu-close-icon");
