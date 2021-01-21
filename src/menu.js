b5ebbe0d4ba03478e0e0de3385d2735841240d53
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2021-01-21 13:25:51 +0530

    updated hamburger menu

--- src/menu.js
+++ src/menu.js
@@ -1,26 +1,32 @@
-function Menu(el, button, icon, close) {
-    this.el = document.querySelector(el);
-    this.button = document.querySelector(button);
-    this.hamburgerMenuIcon = document.querySelector(icon);
-    this.closeIcon = document.querySelector(close);
-    this.displayMenu = false;
-    this.bindEvents();
+class Menu {
+    constructor(menuBtn, exitBtn, nav) {
+        this.menuBtn = this.getElement(menuBtn);
+        this.exitBtn = this.getElement(exitBtn);
+        this.nav = this.getElement(nav);
+
+        console.log(this.menuBtn);
+        console.log(this.exitBtn);
+        console.log(this.nav);
+
+        this.bindEvents();
+    }
+
+    bindEvents() {
+        this.menuBtn.addEventListener("click", () => {
+            this.nav.classList.add("open-nav");
+            this.menuBtn.style.display = "none";
+            this.exitBtn.style.display = "block";
+        });
+        this.exitBtn.addEventListener("click", () => {
+            this.nav.classList.remove("open-nav");
+            this.menuBtn.style.display = "block";
+            this.exitBtn.style.display = "none";
+        });
+    }
+
+    getElement(element) {
+        return document.querySelector(element);
+    }
 }
 
-Menu.prototype.bindEvents = function () {
-    this.button.addEventListener("click", () => {
-        if (this.displayMenu === false) {
-            this.el.style.display = "flex";
-            this.displayMenu = true;
-            this.hamburgerMenuIcon.style.display = "none";
-            this.closeIcon.style.display = "block";
-        } else {
-            this.el.style.display = "none";
-            this.displayMenu = false;
-            this.hamburgerMenuIcon.style.display = "block";
-            this.closeIcon.style.display = "none";
-        }
-    });
-};
-
-new Menu("#menu", "#hamburger", ".hamburger-icon", ".menu-close-icon");
+new Menu("#menu-btn", "#exit-btn", "#nav");
